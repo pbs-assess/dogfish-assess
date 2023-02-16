7907ceb487e320a9c55cc0abdc0e7013a9755970
Sean Anderson <sean@seananderson.ca>
2023-02-15 17:04:39 -0800

    Bring in historical catch

--- analysis/02-catch.R
+++ analysis/02-catch.R
@@ -1,10 +1,15 @@
 library(dplyr)
 library(gfplot)
+library(ggplot2)
+source("analysis/utils.R")
+
+# modern catches ------------------------------------------------------
 
 if (Sys.info()[["user"]] == "seananderson") {
   d <- readRDS("data/raw/catch.rds")
   table(d$major_stat_area_name)
-  d_4b5abcde <- tidy_catch(d, areas = c("5[CDE]+", "5[AB]+", "4B"))
+  # d_4b5abcde <- tidy_catch_dogfish(d, areas = c("5[CDE]+", "5[AB]+", "4B"))
+  d_4b5abcde <- tidy_catch_dogfish(d, areas = c("5[ABCDE]+", "4B"))
   saveRDS(d_4b5abcde, file = "data/generated/catch-4B5ABCDE-summarized.rds")
 }
 
@@ -13,4 +18,120 @@ if (Sys.info()[["user"]] == "seananderson") {
 
 # cached and force-pushed to GitHub:
 d_4b5abcde <- readRDS("data/generated/catch-4B5ABCDE-summarized.rds")
-plot_catch(d_4b5abcde)
+# plot_catch(d_4b5abcde)
+
+# Trust trawl >= 1996
+# Trust longline >= 2007
+
+d_4b5abcde_trawl <- d_4b5abcde |>
+  filter(year >= 1996 & gear %in% c("Bottom trawl", "Midwater trawl"))
+
+d_4b5abcde_ll <- d_4b5abcde |>
+  filter(year >= 2007 & gear %in% c("Hook and line", "Trap", "Unknown/trawl", "Discarded"))
+
+d_catch_modern <- bind_rows(d_4b5abcde_trawl, d_4b5abcde_ll)
+
+# plot_catch(d_4b5abcde_ll)
+# plot_catch(d_4b5abcde_trawl)
+
+# historical catches --------------------------------------------------
+
+landings_1935 <- readr::read_csv("data/raw/catches-all-gears-1935-1965.csv", comment = "#")
+landings_1966_ll <- readr::read_csv("data/raw/catches-longline-1966-2008.csv", comment = "#")
+landings_1966_trawl <- readr::read_csv("data/raw/catches-trawl-1966-2008.csv", comment = "#")
+discards_1966_trawl <- readr::read_csv("data/raw/discards-trawl-1966-2008.csv", comment = "#")
+discards_2001_ll <- readr::read_csv("data/raw/discards-longline-2001-2006.csv", comment = "#")
+
+make_NAs_zero <- function(x) {
+  for (i in seq_along(names(x))) {
+    x[[i]][is.na(x[[i]])] <- 0
+  }
+  x
+}
+landings_1935 <- make_NAs_zero(landings_1935)
+landings_1966_ll <- make_NAs_zero(landings_1966_ll)
+landings_1966_trawl <- make_NAs_zero(landings_1966_trawl)
+discards_1966_trawl <- make_NAs_zero(discards_1966_trawl)
+discards_2001_ll <- make_NAs_zero(discards_2001_ll)
+
+# Percent not assigned !?
+d_1935 <- landings_1935 |>
+  transmute(year = Year, `4B` = `4B`, `5ABCDE` = Total)
+d_1935 <- tidyr::pivot_longer(
+  d_1935, -1, names_to = "area", values_to = "landed_kg") |>
+  mutate(gear = "Trawl + hook and line", landed_kg = landed_kg * 1000)
+
+# assume unknown area is outside: !? TODO
+d_1966_ll <- landings_1966_ll |>
+  transmute(year = Year, `4B` = `4B`, `5ABCDE` = `Total outside` + `Unknown area`)
+d_1966_ll <- tidyr::pivot_longer(
+  d_1966_ll, -1, names_to = "area", values_to = "landed_kg") |>
+  mutate(gear = "Hook and line", landed_kg = landed_kg * 1000) |>
+  filter(year < 2007)
+
+# assume unknown area is outside: !? TODO
+d_1966_tr <- landings_1966_trawl |>
+  transmute(year = Year, `4B` = `4B`, `5ABCDE` = `Total` + `Unknown area`)
+d_1966_tr <- tidyr::pivot_longer(
+  d_1966_tr, -1, names_to = "area", values_to = "landed_kg") |>
+  mutate(gear = "Trawl", landed_kg = landed_kg * 1000) |>
+  filter(year < 1996)
+
+disc_1966_tr <- discards_1966_trawl |>
+  transmute(year = Year, `4B` = `4B`, `5ABCDE` = `Total`)
+disc_1966_tr <- tidyr::pivot_longer(
+  disc_1966_tr, -1, names_to = "area", values_to = "discarded_kg") |>
+  mutate(gear = "Trawl", discarded_kg = discarded_kg * 1000) |>
+  filter(year < 1996)
+
+# assume unknown area is outside: !? TODO
+disc_2001_ll <- discards_2001_ll |>
+  transmute(year = Year, `4B` = `4B`, `5ABCDE` = `Total` + `Unknown area`)
+disc_2001_ll <- tidyr::pivot_longer(
+  disc_2001_ll, -1, names_to = "area", values_to = "discarded_kg") |>
+  mutate(gear = "Hook and line", discarded_kg = discarded_kg * 1000)
+
+disc <- bind_rows(disc_1966_tr, disc_2001_ll)
+
+ggplot(disc, aes(x = year, y = discarded_kg, colour = gear, fill = gear)) +
+  facet_wrap(~area, scales = "fixed") +
+  geom_col()
+
+old_landings <- bind_rows(list(
+  d_1935,
+  d_1966_ll,
+  d_1966_tr
+))
+
+ggplot(old_landings, aes(x = year, y = landed_kg, colour = gear, fill = gear)) +
+  facet_wrap(~area, scales = "fixed") +
+  geom_col()
+
+old_dat <- full_join(old_landings, disc)
+old_dat <- make_NAs_zero(old_dat)
+
+ggplot(old_dat, aes(x = year, y = landed_kg + discarded_kg, colour = gear, fill = gear)) +
+  facet_wrap(~area, scales = "fixed") +
+  geom_col()
+
+d <- bind_rows(d_catch_modern, old_dat)
+
+catch_plot <- function(x, column) {
+  gears <- sort(unique(x$gear))
+  cols <- RColorBrewer::brewer.pal(n = length(gears), name = "Dark2")
+  names(cols) <- gears
+  ggplot(d, aes(x = year, y = {{ column }},  fill = gear)) +
+    facet_wrap(~area, scales = "fixed") +
+    geom_col(colour = "grey50", linewidth = 0.5) +
+    coord_cartesian(expand = FALSE) +
+    theme_pbs() +
+    scale_fill_manual(values = cols) +
+    ylim(0, 2.5e04)
+}
+
+land <- catch_plot(d, landed_kg/1000) + ggtitle("Landings")
+discard <- catch_plot(d, discarded_kg/1000) + ggtitle("Discards")
+catch <- catch_plot(d, landed_kg/1000 + discarded_kg/1000) + ggtitle("Catch")
+cowplot::plot_grid(plotlist = list(land, discard, catch), ncol = 1L)
+
+saveRDS(catch, file = "data/generated/catch.rds")
---
+++ analysis/utils.R
@@ -0,0 +1,52 @@
+tidy_catch_dogfish <- function(
+    dat,
+    areas = NULL,
+    ...
+) {
+  dat <- set_fishing_year(dat, ...)
+
+  if (!is.null(areas)) {
+    dat$area <- assign_areas(dat$major_stat_area_name, areas)
+    dat <- dat[!is.na(dat$area), , drop = FALSE]
+  } else {
+    dat$area <- "Coastwide"
+  }
+
+  dat <- filter(dat, !is.na(species_common_name), !is.na(year)) %>%
+    group_by(year, species_common_name, gear, area) %>%
+    summarise(
+      landed_kg = sum(landed_kg, na.rm = TRUE),
+      discarded_kg = sum(discarded_kg, na.rm = TRUE),
+      landed_pcs = sum(landed_pcs, na.rm = TRUE),
+      discarded_pcs = sum(discarded_pcs, na.rm = TRUE),
+      .groups = "drop"
+    ) %>%
+    arrange(species_common_name, year)
+
+  # Note that discarded_kg only includes trawl discards from 1996+ and
+  # trap/hook and line discards from 2006+
+  catches <- mutate(dat,
+    gear = dplyr::recode(gear,
+      UNKNOWN = "Unknown/trawl",
+      `BOTTOM TRAWL` = "Bottom trawl",
+      `HOOK AND LINE` = "Hook and line",
+      `LONGLINE` = "Hook and line",
+      `MIDWATER TRAWL` = "Midwater trawl",
+      `TRAP` = "Trap",
+      `UNKNOWN TRAWL` = "Unknown/trawl"
+    )
+  ) %>%
+    select(year, area, species_common_name, gear, landed_kg, discarded_kg)
+
+  # cm <- reshape2::melt(catches,
+  #   id.vars = c("year", "species_common_name", "area", "gear")
+  # )
+
+  all_catch <- group_by(catches, year, species_common_name, area, gear) %>%
+    summarise(
+      landed_kg = sum(landed_kg, na.rm = TRUE),
+      discarded_kg = sum(discarded_kg, na.rm = TRUE),
+      .groups = "drop")
+
+  all_catch
+}
