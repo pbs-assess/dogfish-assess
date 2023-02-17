06a8fa6b0ecd06609222dfbd93dada1f976d36ca
Sean Anderson <sean@seananderson.ca>
2023-02-17 12:57:02 -0800

    Editing IPHC script

--- analysis/01-iphc-index.R
+++ analysis/01-iphc-index.R
@@ -1,39 +1,42 @@
-
-#library
 library(ggplot2)
 library(dplyr)
 library(sdmTMB)
 
-
-# Retrieve survey data
-s <- readRDS("data/IPHC_coastdata.rds") %>% #outside only, downloaded from website, expansion set and SOG removed
-     dplyr::filter(iphc.reg.area == "2B")
+s <- readRDS("data/raw/IPHC_coastdata.rds") %>%
+  # outside only, downloaded from website, expansion set and SOG removed:
+  dplyr::filter(iphc.reg.area == "2B")
 d <- sdmTMB::add_utm_columns(s, ll_names = c("beginlon", "beginlat"), utm_crs = 32609)
+d$hooksobserved <- as.numeric(d$hooksobserved)
+names(d) <- gsub("\\.", "_", names(d))
 
 glimpse(d)
 sort(unique(d$year))
-unique(d)
 
 ggplot(d, aes(X, Y, size = cpue)) +
   geom_point(pch = 21, alpha = 0.3) +
   facet_wrap(vars(year)) +
   coord_fixed()
 
-sum(is.na(d$depth_m))
-d <- dplyr::filter(d, !is.na(depth_m_log))
-d$log_hookobserved <- log(as.numeric(d$hooksobserved))
+stopifnot(sum(is.na(d$depth_m)) == 0L)
+stopifnot(sum(is.na(d$depth_m_log)) == 0L)
+stopifnot(sum(is.na(d$number_observed)) == 0L)
+stopifnot(sum(is.na(d$hooksobserved)) == 0L)
+range(d$number_observed)
+range(d$hooksobserved)
+range(d$depth_m_log)
+d$log_hookobserved <- log(d$hooksobserved)
 
-mesh <- make_mesh(d, c("X", "Y"), cutoff = 12)
+mesh <- make_mesh(d, c("X", "Y"), cutoff = 15)
 plot(mesh)
 mesh$mesh$n
 
-#models
 fit_iphc_nb2 <- sdmTMB(
-  cpue ~ 1 + poly(depth_m_log, 2L),
+  number_observed ~ 1 + poly(depth_m_log, 2L),
   family = nbinom2(link = "log"),
   data = d,
   mesh = mesh,
   time = "year",
+  offset = "log_hookobserved",
   spatiotemporal = "rw",
   spatial = "on",
   silent = FALSE,
@@ -43,30 +46,30 @@ fit_iphc_nb2 <- sdmTMB(
 saveRDS(fit_iphc_nb2, file = "data/generated/iphc-nb2-sdmTMB.rds")
 fit_iphc_nb2 <- readRDS("data/generated/iphc-nb2-sdmTMB.rds")
 
-fit_iphc_tw <- update(fit_iphc_nb2, family = tweedie())
+# fit_iphc_tw <- update(fit_iphc_nb2, family = tweedie())
 
-sanity(fit_iphc_tw)
+# sanity(fit_iphc_tw)
 sanity(fit_iphc_nb2)
 
-plot_anisotropy(fit_iphc_tw)
+# plot_anisotropy(fit_iphc_tw)
 plot_anisotropy(fit_iphc_nb2)
 
-fit_iphc_tw
+# fit_iphc_tw
 
-fit_iphc_tw$sd_report
+# fit_iphc_tw$sd_report
 fit_iphc_nb2$sd_report
 
-AIC(fit_iphc_tw, fit_iphc_nb2)
+# AIC(fit_iphc_tw, fit_iphc_nb2)
 
-
-
-#grid if IPHC main fixed survey locations
-s <- readRDS("data/IPHC_coastdata.rds") %>% #outside only, downloaded from website, expansion set and SOG removed
-  dplyr::filter(iphc.reg.area == "2B") %>% distinct(station, .keep_all = TRUE)
-grid <- s %>% dplyr::select(beginlon, beginlat) %>% distinct(.keep_all = TRUE)
+# grid of IPHC main fixed survey locations
+s <- readRDS("data/IPHC_coastdata.rds") %>%
+  # outside only, downloaded from website, expansion set and SOG removed
+  dplyr::filter(iphc.reg.area == "2B") %>%
+  distinct(station, .keep_all = TRUE)
+grid <- s %>% dplyr::select(beginlon, beginlat) %>%
+  distinct(.keep_all = TRUE)
 g <- add_utm_columns(grid, ll_names = c("beginlon", "beginlat"), utm_crs = 32609)
 plot(g$beginlon, g$beginlat)
-
 
 ggplot(g, aes(X, Y), fill = depth_m, colour = depth_m) +
   geom_tile(width = 2, height = 2) +
@@ -77,27 +80,21 @@ ggplot(g, aes(X, Y), fill = depth_m, colour = depth_m) +
 year <- sort(union(unique(d$year), fit_ins_nb2$extra_time))
 grid <- purrr::map_dfr(year, function(.x) {dplyr::mutate(g, year = .x)})
 
-p_ins_tw <- predict(fit_iphc_tw, newdata = grid, return_tmb_object = TRUE)
-ind_ins <- get_index(p_ins_tw, bias_correct = TRUE)
+p_ins <- predict(fit_iphc_nb2, newdata = grid, return_tmb_object = TRUE)
+ind_ins <- get_index(p_ins, bias_correct = TRUE)
 survs <- select(d, year, survey_abbrev) |> distinct()
 ind_ins <- left_join(ind_ins, survs, by = join_by(year))
 
-ggplot(ind, aes(year, est, ymin = lwr, ymax = upr, colour = survey_abbrev)) +
+ggplot(ind_ins, aes(year, est, ymin = lwr, ymax = upr, colour = survey_abbrev)) +
   geom_pointrange() +
   coord_cartesian(ylim = c(0, NA))
 
-ind_ins_save <- dplyr::filter(ind_ins, !is.na(survey_abbrev))
-saveRDS(ind_ins_save, file = "data/generated/geostat-ind-iphc.rds")
+saveRDS(ind_ins, file = "data/generated/geostat-ind-iphc.rds")
 ind_ins_save <- readRDS("data/generated/geostat-ind-iphc.rds")
 
 ggplot(ind_ins_save, aes(year, est, ymin = lwr, ymax = upr, colour = survey_abbrev)) +
   geom_pointrange() +
   coord_cartesian(ylim = c(0, NA))
-
-
-
-
-
 
 
 both2 <- filter(both, survey_abbrev == "HBLL INS N" | survey_abbrev == "HBLL INS S")
