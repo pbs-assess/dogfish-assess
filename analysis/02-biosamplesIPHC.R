b876a89cdd7795bfd59156461eaee255247f144d
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2023-02-22 13:50:54 -0800

    exploratory figures

--- analysis/02-biosamplesIPHC.R
+++ analysis/02-biosamplesIPHC.R
@@ -1,7 +1,25 @@
 library(dplyr)
 
-iphclengths <- read.csv("C:/Dogfish_stitch/IPHC data from NOAA/IPHC_dogfish_lengths2021.csv") %>%
-  filter(reg_area == "2B"
+d <- read.csv("data/raw/IPHC_dogfish_lengths2021.csv") %>%
+  filter(reg_area == "2B", sex %in% c("F", "M")
   )
-glimpse(iphclengths)
+glimpse(d)
 
+unique(d$reg_area)
+unique(d$sex)
+sort(unique(d$year))
+range(na.omit(d$length)) #units are cm
+
+#Exploratory figures
+d %>% group_by(year, sex) %>%
+  summarize(count_sex = n()) %>%
+  ggplot() +
+  geom_line(aes(year, count_sex, group = sex, col = sex)) +
+  geom_point(aes(year, count_sex, group = sex, col = sex))
+
+d %>%
+  drop_na(length) %>%
+  ggplot() +
+  geom_jitter(aes(as.factor(year), length, group = sex, col = sex), alpha = 0.05) +
+  geom_boxplot(aes(as.factor(year), length, col = sex)) +
+  facet_wrap(~sex)
