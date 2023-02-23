84e434270a8234db5ebad25bd1eee5cf74679ecc
Sean Anderson <sean@seananderson.ca>
2023-02-22 16:56:51 -0800

    Combine indices

--- analysis/02-survey-indexes.R
+++ analysis/02-survey-indexes.R
@@ -3,14 +3,16 @@ library(ggplot2)
 
 ind_hbll_out <- readRDS("data/generated/geostat-ind-hbll-out.rds")
 ind_syn_out <- readRDS("data/generated/geostat-ind-synoptic.rds")
+ind_iphc_out <- readRDS("data/generated/geostat-ind-iphc.rds")
 
 bind_rows(
-  mutate(ind_hbll_out, survey = "HBLL OUT"),
-  mutate(ind_syn_out, survey = "Synoptic trawl")
+  mutate(ind_hbll_out, survey = "HBLL - abundance"),
+  mutate(ind_syn_out, survey = "Synoptic trawl - biomass"),
+  mutate(ind_iphc_out, survey = "IPHC - abundance")
 ) |>
   ggplot(aes(year, est, ymin = lwr, ymax = upr)) +
   geom_pointrange() +
-  facet_wrap(~survey, scales = "free_y") +
+  facet_wrap(~survey, scales = "free_y", ncol = 1L) +
   coord_cartesian(ylim = c(0, NA)) +
   ylab("Relative abundance or biomass") +
   xlab("Year") +
