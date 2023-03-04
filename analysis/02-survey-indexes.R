0c71f10196033c2a9db95dcbf54a897c1bb46a75
ecophilina <paenglis@lakeheadu.ca>
2023-03-03 16:39:00 -0800

    add split sex indexes for trawl

--- analysis/02-survey-indexes.R
+++ analysis/02-survey-indexes.R
@@ -2,16 +2,25 @@ library(dplyr)
 library(ggplot2)
 
 ind_hbll_out <- readRDS("data/generated/geostat-ind-hbll-out.rds")
-ind_syn_out <- readRDS("data/generated/geostat-ind-synoptic.rds")
+# ind_syn_out <- readRDS("data/generated/geostat-ind-synoptic.rds")
 ind_iphc_out <- readRDS("data/generated/geostat-ind-iphc.rds")
+ind_syn_out_f <- readRDS("data/generated/geostat-ind-synoptic-female.rds")
+ind_syn_out_m <- readRDS("data/generated/geostat-ind-synoptic-male.rds")
 
 bind_rows(
-  mutate(ind_hbll_out, survey = "HBLL - abundance"),
-  mutate(ind_syn_out, survey = "Synoptic trawl - biomass"),
-  mutate(ind_iphc_out, survey = "IPHC - abundance")
+  mutate(ind_hbll_out, survey = "HBLL - abundance",
+         Index = "Total"),
+  # mutate(ind_syn_out, survey = "Synoptic trawl - biomass"),
+  mutate(ind_syn_out_f, survey = "Synoptic trawl - biomass",
+                        Index = "Female"),
+  mutate(ind_syn_out_m, survey = "Synoptic trawl - biomass",
+                        Index = "Male"),
+  mutate(ind_iphc_out, survey = "IPHC - abundance",
+         Index = "Total")
 ) |>
-  ggplot(aes(year, est, ymin = lwr, ymax = upr)) +
+  ggplot(aes(year, est, ymin = lwr, ymax = upr, colour = Index)) +
   geom_pointrange() +
+  scale_colour_manual(values = c("red",  "blue", "black")) +
   facet_wrap(~survey, scales = "free_y", ncol = 1L) +
   coord_cartesian(ylim = c(0, NA)) +
   ylab("Relative abundance or biomass") +
