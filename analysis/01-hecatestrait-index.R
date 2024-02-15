1a7f08b64d39c36dc71c9a0bb0efea8d3e8f7644
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-02-15 10:40:17 -0800

    init commit

---
+++ analysis/01-hecatestrait-index.R
@@ -0,0 +1,13 @@
+#calculate a coastwide index using the Hecate Strait Multi Species trawl survey
+
+
+# library -----------------------------------------------------------------
+
+
+# load data ---------------------------------------------------------------
+
+
+
+# create grid -------------------------------------------------------------
+
+#can either be a convex polygon
