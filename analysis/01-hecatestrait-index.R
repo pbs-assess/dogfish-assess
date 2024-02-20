9c5b8139c4b19360e21c1abb057e517b3cee706b
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-02-20 13:33:50 -0800

    extra time

--- analysis/01-hecatestrait-index.R
+++ analysis/01-hecatestrait-index.R
@@ -1,11 +1,8 @@
-#calculate a coastwide index using the Hecate Strait Multi Species trawl survey
+#calculate a Hecate Strait multi species
 
 #Hecate Strait multi species trawl extends back to 1984
-#can either predict along the whole coast to create one index OR
+#here I create an index for the HS MS trawl
 #add a multiplier to add standard error to the coast wide index
-
-#the grid can be a convex polygon with a 2 by 2 grid or
-#overlay the current grid with a convex polygon
 
 # library -----------------------------------------------------------------
 library(dplyr)
@@ -71,47 +68,9 @@ d |>
 
 
 
-# data cleaning ignore for now -----------------------------------------------------------
-# glimpse(d)
-# #effort mutate(area_swept1_m2 = doorspread_m * tow_length_m) |>
-#   drop_na(doorspread_m)ignore
-#
-# #included some sets unsually not deemed usable
-# meandoors <- dat %>%
-#   filter(usability_code == 1 &
-#            doorspread_m != 0) %>%
-#   group_by(survey_id) %>% summarise(
-#     mean_doorspread = mean(doorspread_m, na.rm = TRUE)
-#   )
-#
-# dat <- dat %>%
-#   filter(usability_code %in% c(1, 22, 16, 6)) %>%
-#   filter(duration_min >= 10) %>%
-#   left_join(meandoors) %>%
-#   mutate(
-#     DOY = as.numeric(strftime(time_deployed, format = "%j")),
-#     days_to_solstice = DOY - 172,
-#     survey_type = as.factor(ifelse(survey_abbrev == "HS MSA", "MSA", "SYN")),
-#     log_depth = log(depth_m),
-#     log_depth_c = log_depth - 5, # mean and median for whole data set
-#     doorspread_m = ifelse(doorspread_m == 0, mean_doorspread, doorspread_m),
-#     area_swept = ifelse(is.na(tow_length_m), doorspread_m * duration_min * speed_mpm, doorspread_m * tow_length_m)
-#   )
-
-
-
-
 # data cleaning -----------------------------------------------------------
 d <- d |>
   mutate(UTM.lon = X, UTM.lat = Y)
-
-# d <- d |>
-#   mutate(area_swept_calc = ifelse(is.na(tow_length_m), doorspread_m * duration_min * speed_mpm,
-#                                   doorspread_m * tow_length_m))
-# d |>
-#   ggplot() +
-#   geom_point(aes(area_swept, area_swept_calc, colour = catch_weight), size = 2)
-
 d$offset <- log(d$area_swept)
 d <- d |>
   mutate(date2 = as.Date(time_deployed, format = "%Y-%m-%d H:M:S")) |>
@@ -119,10 +78,12 @@ d <- d |>
   mutate(julian = lubridate::yday(dmy))
 d <- d |>
   mutate(logbotdepth_m = log(depth_m))
+d <- d |> mutate(logbot_depth = log(depth_m), logbot_depth2 = log(depth_m) * log(depth_m))
+d$julian_small <- d$julian/100
 
 d |>
   group_by(year) |>
-  summarize(catch = sum(catch_weight), effort = sum(area_swept_calc)) |>
+  summarize(catch = sum(catch_weight), effort = sum(area_swept)) |>
   mutate(cpue = catch/effort) |>
   ggplot() +
   geom_point(aes(year, catch)) +
@@ -130,7 +91,7 @@ d |>
 
 d |>
   group_by(year) |>
-  summarize(catch = sum(catch_weight), effort = sum(area_swept_calc)) |>
+  summarize(catch = sum(catch_weight), effort = sum(area_swept)) |>
   mutate(cpue = catch/effort) |>
   ggplot() +
   geom_point(aes(year, cpue)) +
@@ -138,7 +99,7 @@ d |>
 
 d |>
   ggplot() +
-  geom_point(aes(year, area_swept_calc, colour = catch_weight), size = 2)
+  geom_point(aes(year, area_swept, colour = catch_weight), size = 2)
 
 d <- d |>
   mutate(date2 = as.Date(time_deployed, format = "%Y-%m-%d H:M:S")) |>
@@ -157,18 +118,20 @@ d |>
 g <- gfplot::synoptic_grid
 #drop year and then add the HS years in
 g <- g |> dplyr::select(-survey_domain_year)
-g <- purrr::map_dfr(unique(d$year), ~ tibble(g, year = .x))
+
+#continuous year or years in the survey only
+#g <- purrr::map_dfr(unique(d$year), ~ tibble(g, year = .x))
+year <- seq(min(d$year), max(d$year), 1)
+g <- purrr::map_dfr(year, ~tibble(g, year = .x))
+
 g$logbot_depth <- log(g$depth)
 g$logbot_depth2 <- log(g$depth) * log(g$depth)
 g <- g |> mutate(UTM.lon = X, UTM.lat = Y)
+g$offset <- 0
 
 plot(g$X, g$Y)
 g_sf <- st_as_sf(g, coords = c("X", "Y"), crs = 32609)
-plot(g_sf)
-
-plot(mesh)
-points(g$X, g$Y) #grid
-points(d$X, d$Y, col = "red") #HS points
+plot(st_geometry(g_sf))
 
 #convex polygon
 d_sf <- st_as_sf(d, coords = c("X", "Y"), crs = 32609)
@@ -183,16 +146,7 @@ grid_hs <- st_intersection(hull_buf, g_sf)
 plot(st_geometry(grid_hs))
 plot(st_geometry(d_sf), col = "red", add = TRUE)
 
-#what are those three points?? they are outside of the grid so maybe I should remove them
-plot(st_geometry(g_sf))
-plot(st_geometry(d_sf), col = "red", add = TRUE)
-
-#what are those three points?? they are outside of the grid so maybe I should remove them
-plot(st_geometry(g_sf))
-plot(st_geometry(d_sf), col = "red", add = TRUE)
-
 grid_sf <- st_as_sf(g, coords = c("X", "Y"), crs = 32609)
-plot(st_geometry(grid_sf))
 grid_hull <- concaveman::concaveman(grid_sf)
 
 d_sf2 <- st_intersection(grid_hull, d_sf)
@@ -214,7 +168,7 @@ mesh <- make_mesh(d_sf2, c("UTM.lon", "UTM.lat"), cutoff = 15)
 plot(mesh)
 mesh$mesh$n
 
-# model -------------------------------------------------------------------
+# HS MSM only model -------------------------------------------------------------------
 #data
 d_sf2
 #grid
@@ -225,23 +179,22 @@ grid_hs$julian_small <- grid_hs$julian/100
 plot(grid_hs$UTM.lon, grid_hs$UTM.lat)
 #mesh
 mesh
-#
-d_sf2 <- d_sf2 |> mutate(logbot_depth = log(depth_m), logbot_depth2 = log(depth_m) * log(depth_m))
-d_sf2$julian_small <- d_sf2$julian/100
 
 
 d_sf2 |>
   group_by(year) |>
-  summarize(catch = sum(catch_weight), effort = sum(area_swept_calc)) |>
+  summarize(catch = sum(catch_weight), effort = sum(area_swept)) |>
   mutate(cpue = catch/effort) |>
   ggplot() +
   geom_point(aes(year, cpue)) +
   geom_line(aes(year, cpue))
 
+unique(d_sf2$year)
+
 m <- sdmTMB::sdmTMB(
   #catch_weight ~ 1 + survey_type +  poly(log_depth_c, 2) + poly(days_to_solstice, 2),
-  #catch_weight ~ 1 + logbot_depth + logbot_depth2,
-  catch_weight ~ 1 + logbot_depth + logbot_depth2 + julian_small,
+  catch_weight ~ 1 + logbot_depth + logbot_depth2,
+  #catch_weight ~ 1 + logbot_depth + logbot_depth2 + poly(julian_small,2),
     data = d_sf2,
     time = "year",
     mesh = mesh,
@@ -252,6 +205,7 @@ m <- sdmTMB::sdmTMB(
     #do_index = FALSE,
     do_index = TRUE,
     share_range = FALSE,
+    #extra_time = c(1985, 1986, 1988, 1990, 1992, 1994, 1997, 1999, 2001),
     predict_args = list(newdata = grid_hs),
     control = sdmTMBcontrol(newton_loops = 1L),
     index_args = list(area = grid_hs$cell_area),
@@ -260,10 +214,13 @@ m <- sdmTMB::sdmTMB(
     family = delta_lognormal() #sensitive to distribution, delta gamma
 
   )
+sanity(m)
+
 saveRDS(m, "data/generated/m_tweedie")
 saveRDS(m, "data/generated/m_HSMSdg")
 saveRDS(m, "data/generated/m_HSMSdl.rds")
 saveRDS(m, "data/generated/m_HSMSdl_julian.rds")
+saveRDS(m, "data/generated/m_HSMSdl_julianex.rds")
 
 m <- readRDS("data/generated/m_tweedie")
 m1 <- readRDS("data/generated/m_HSMSdg")
@@ -274,6 +231,7 @@ index <- get_index(m, bias_correct = TRUE)
 indexdg <- get_index(m1, bias_correct = TRUE)
 indexdl <- get_index(m2, bias_correct = TRUE)
 indexdljulian <- get_index(m3, bias_correct = TRUE)
+
 
 #pred <- predict(m, grid_hs, return_tmb_object = TRUE, response = TRUE)
 
@@ -288,9 +246,11 @@ ggplot(all, aes(year, est, colour = type)) + geom_point() + geom_line() +
   geom_ribbon(aes(ymin = lwr, ymax = upr), alpha = 0.4, fill = "#8D9999") +
   facet_wrap(~type, scales = "free")
 
-#priors = sdmTMBpriors(
-#  matern_s = pc_matern(range_gt = cutoff_distance * 1.5,
-#                       sigma_lt = 2),
-#  matern_st = pc_matern(range_gt = cutoff_distance * 1.5,
-#                        sigma_lt = 2)
-#)
+dl <- indexdl |> mutate(type = "dl")
+dl_julian <- indexdljulian |> mutate(type = "dl_julian")
+
+dl <-rbind(dl, dl_julian)
+
+ggplot(dl, aes(year, est, colour = type)) + geom_point() + geom_line() +
+  geom_ribbon(aes(ymin = lwr, ymax = upr), alpha = 0.4, fill = "#8D9999")
+
