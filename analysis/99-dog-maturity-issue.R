110634a7944f4a23640a93a58b344d90ed55b348
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-09-13 11:28:51 -0700

    rm mature female > 95 and immature

--- analysis/99-dog-maturity-issue.R
+++ analysis/99-dog-maturity-issue.R
@@ -1,6 +1,7 @@
 library(gfplot)
 library(tidyr)
 library(ggplot2)
+library(dplyr)
 theme_set(theme_pbs())
 
 
@@ -52,16 +53,21 @@ summary(fit$model)
 plot_mat_ogive(fit) #f50 is 97.6
 
 #test if we define females > 90 cm at mature what happens to the F50
-test <- d |> mutate(maturity_code = ifelse(sex == 2 & length > 90, 77, maturity_code))
-test2 <- test |> mutate(matbin = ifelse(sex == 1 & maturity_code <30, "immature", ifelse(sex == 1 & maturity_code >= 30, "mature", ifelse(sex ==2 & maturity_code < 77, "immature", ifelse(sex ==2 & maturity_code >=77, "mature", NA)))))
-test2 |> filter(sex %in% c(1,2)) |>
+#rm potentially misclassified large females
+#test <- d |> mutate(maturity_code = ifelse(sex == 2 & length > 90, 77, maturity_code))
+test <- d |> mutate(matbin = ifelse(sex == 1 & maturity_code <30, "immature", ifelse(sex == 1 & maturity_code >= 30, "mature", ifelse(sex ==2 & maturity_code < 77, "immature", ifelse(sex ==2 & maturity_code >=77, "mature", NA)))))
+test2 <- test |> filter(sex == 2 & length > 90 & matbin == "immature")
+test  <- filter(test, !test$specimen_id %in% test2$specimen_id)
+
+test |>
+  filter(sex %in% c(1,2)) |>
   drop_na(maturity_code) |>
   filter(maturity_code != 0) |>
   ggplot(aes((matbin), length, colour = (matbin))) +
   geom_jitter() +
   facet_wrap(~sex)
 
-fit2 <- test2 |>
+fit2 <- test |>
   filter(survey_abbrev %in% c("SYN WCHG", "SYN HS", "SYN QCS", "SYN WCVI")) |>
   fit_mat_ogive(type = "length", usability_codes = c(0, 1, 2, 6),
                 custom_maturity_at = c(30, 77))
