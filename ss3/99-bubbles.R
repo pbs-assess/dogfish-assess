6cff44a6f1e85f0e8f852bd60a4d255ac88fc26f
Sean Anderson <sean@seananderson.ca>
2025-01-01 19:19:26 -0800

    Bubbles

--- ss3/99-bubbles.R
+++ ss3/99-bubbles.R
@@ -1,9 +1,18 @@
-d <- readRDS("~/Downloads/SS3_N_A0.rds")
-
 library(ggplot2)
 library(dplyr)
 
-d |>
+# d <- readRDS("~/Downloads/SS3_N_A0.rds")
+
+dd <- r4ss::SS_output("ss3/A0/")
+dd <- dd$natage
+age <- seq(0, 60, 10)
+out <- dd |>
+  filter(Era == "TIME", `Beg/Mid` == "B") %>%
+  select(Yr, Sex, as.character(age)) %>%
+  reshape2::melt(id.vars = c("Yr", "Sex")) %>%
+  mutate(Sex = ifelse(Sex == 1, "Female", "Male"))
+
+out |>
   ggplot(aes(Yr, variable, size = value, colour = value)) +
   geom_point(pch = 21) +
   geom_point(pch = 19, alpha = 0.07) +
@@ -13,6 +22,9 @@ d |>
   facet_wrap(~Sex) +
   scale_size_area(max_size = 12) +
   guides(colour = "none", size = "none") +
-  coord_cartesian(expand = FALSE, xlim = range(d$Yr), ylim = c(0.8, 6.2)) +
-  scale_x_continuous(breaks = seq(1940, 2020, 10))
-ggsave("figs/bubble.png", width = 9, height = 3.5)
+  coord_cartesian(expand = FALSE, xlim = range(out$Yr), ylim = c(0.8, 6.2)) +
+  scale_x_continuous(breaks = seq(1940, 2020, 10)) +
+  scale_colour_viridis_c() +
+  gfplot::theme_pbs()
+source("ss3/99-utils.R")
+ggsave_optipng("figs/bubble.png", width = 9, height = 3.5)
