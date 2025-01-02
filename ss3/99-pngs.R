9a046109dd9993fe7028d20f56f292ffc610ce93
Sean Anderson <sean@seananderson.ca>
2025-01-02 12:47:42 -0800

    Update 99-pngs.R

--- ss3/99-pngs.R
+++ ss3/99-pngs.R
@@ -3,20 +3,22 @@ files_per_core <- 5
 setwd("figs")
 system(paste0(
   "find -X . -name '*.png' -print0 | xargs -0 -n ",
-  files_per_core, " -P ", cores, " /opt/homebrew/bin/optipng -strip all"
+  files_per_core, " -P ", cores, " optipng -strip all"
 ))
 setwd(here::here())
 
-if (FALSE) {
+# if (FALSE) {
   system('rsync -av "figs/ss3/" "../dogfish-assess-resdoc/figs/ss3"')
+  system('rsync -av "figs/" "../dogfish-assess-resdoc/figs"')
   system('rsync -av "values/" "../dogfish-assess-resdoc/values"')
-}
+  system('rsync -av "tables/" "../dogfish-assess-resdoc/tables"')
+# }
 
 if (FALSE) {
   setwd("../dogfish-assess-resdoc/figs/")
   system(paste0(
     "find -X . -name '*.png' -print0 | xargs -0 -n ",
-    files_per_core, " -P ", cores, " /opt/homebrew/bin/optipng -strip all"
+    files_per_core, " -P ", cores, " optipng -strip all"
   ))
   setwd(here::here())
 }
